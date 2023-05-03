6c1795c205731b4e8e853754b06ccfbf0d1ca366
FACU CUENCA <117240275+facucuenca@users.noreply.github.com>
2023-05-02 22:17:33 -0300

    avanzando con el proyecto final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,17 @@ import { SocialComponent } from './components/social/social.component';
     HeaderComponent,
     BannerComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({  })
   ],
   providers: [],
   bootstrap: [AppComponent]
